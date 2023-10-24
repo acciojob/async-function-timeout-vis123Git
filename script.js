4999567eb7c10184411e984f8409ded1a08c75ac
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2023-10-24 19:46:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById("btn").addEventListener("click", displayDelayedMessage);
 async function displayDelayedMessage() {
     const text = document.getElementById("text").value;
     const delay = parseInt(document.getElementById("delay").value) * 1000; 
-    document.getElementById("output").textContent = "Waiting...";
+    document.getElementById("output").textContent = "";
     await new Promise(resolve => setTimeout(resolve, delay));
     document.getElementById("output").textContent = text;
 }

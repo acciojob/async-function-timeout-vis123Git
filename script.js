ca902abec2e2ddce41f22b509e423c45368b5972
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2023-10-24 19:31:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+let textField =  document.getElementById("text").value;
+let timeOut = parseInt(document.getElementById("delay").value);
+let timeButton = document.getElementById("btn");
+let displayOutput = document.getElementById("output");
+
+
+const displayMessage = async (textField,timeOut)=>{
+	try {
+		setTimeOut(()=>{
+			displayOutput.innerText = textField;
+		},timeOut)
+	} catch (error) {
+		
+	}
+}
+timeButton.addEventListener("click", displayMessage())

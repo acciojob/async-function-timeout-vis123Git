0131cd5f331d0c03029bc6850d3232bfb8157d27
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2023-10-27 08:28:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-
 document.getElementById("btn").addEventListener("click", displayDelayedMessage);
-    document.getElementById("output").textContent = "";
+document.getElementById("output").textContent = "";
 
 async function displayDelayedMessage() {
     const text = document.getElementById("text").value;
-    const delay = parseInt(document.getElementById("delay").value) * 1000; 
-    await new Promise(resolve => setTimeout(resolve, delay));
-    document.getElementById("output").textContent = text;
+    const delay = parseInt(document.getElementById("delay").value) * 1000;
+
+    try {
+        if (text.trim() !== "") {
+            await new Promise((resolve) => setTimeout(resolve, delay));
+            document.getElementById("output").textContent = text;
+        }
+    } catch (error) {
+        console.error("An error occurred:", error);
+    }
 }
